40f8d0be19f433427e91ccfba2e11f4ac7f267d3
maxwell <maxwell.agger@gmail.com>
2021-04-26 02:56:51 +1200

    this is first commit

---
+++ assignment.js
@@ -0,0 +1,49 @@
+console.log("this is the first time using javascript");
+console.log("this the second message");
+var name="maxwell" /*this is a string*/
+const age=1
+function addnumbers(a, b){
+    var sum=a+b 
+    console.log(sum);
+}
+addnumbers(2,3)
+function multiplynumbers(a, b, c){
+    var multiply=a*b*c
+    console.log(multiply);
+}
+/*multiplynumbers(2,4,5)
+
+function mynam() {
+    var firstname="maxwell"
+    var lastname="owusu"
+    console.log(firstname+lastname);
+}
+myname()*/
+
+const whereyoustay= (a, b) => {
+    console.log(a+ b)    
+}
+whereyoustay("tafo", "plt 4")
+console.log(``);
+
+var statement1= "my name is "
+var statement =" maxwell owusu"
+console.log(`${statement1} ${statement}`);
+
+function bottle() {
+    var sum=1+2
+return sum;
+
+var cup=bottles();
+console.log(cup)}
+
+var num=78;
+if ( num==`78`){
+    console.log(`true`);
+}
+else{
+    console.log(`false`);
+}
+var firstname="maxwell"
+var lastname="owusu"
+
